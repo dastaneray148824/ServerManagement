a6a8aad0831c7e1614b6cd5032120dab81fe6ad2
dastaneray1234 <dastaneray123@hotmail.com>
2025-04-17 16:24:46 +0300

    Proje dosyası ekle.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            //0 ise false değil ise true döndür
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        [Required]
+        //bir alanın boş geçirilemeyeceğini belirtir
+        public string? Name { get; set; }
+        [Required] public string? City { get; set; }
+
+
+    }
+}
+    
